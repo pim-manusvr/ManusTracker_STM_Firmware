90e3b3bab9d997ad43149164ebb48365f37567d1
Pim Ostendorf <pimostendorf@manus-vr.com>
2020-10-16 11:05:59 +0200

    pre timer 14 backup

--- Inc/main.h
+++ Inc/main.h
@@ -54,7 +54,7 @@ extern "C" {
 void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
-
+void resetDMAIT(uint32_t lookupTable);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
--- Src/main.c
+++ Src/main.c
@@ -26,45 +26,19 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-uint16_t f_updateLed = 0;
-static uint8_t read_r = 0;
-static uint8_t read_g = 0;
-static uint8_t read_b = 0;
+#include "states.h"
 
-uint32_t SineLookup[512] = {
-512,518,524,530,537,543,549,555,562,568,574,580,587,593,599,605,
-611,617,624,630,636,642,648,654,660,666,672,678,684,690,696,701,
-707,713,719,725,730,736,741,747,753,758,764,769,774,780,785,790,
-796,801,806,811,816,821,826,831,836,841,846,850,855,860,864,869,
-873,878,882,886,890,895,899,903,907,911,915,919,922,926,930,933,
-937,940,944,947,950,953,957,960,963,966,968,971,974,977,979,982,
-984,986,989,991,993,995,997,999,1001,1003,1004,1006,1008,1009,1011,1012,
-1013,1014,1015,1017,1017,1018,1019,1020,1021,1021,1022,1022,1022,1023,1023,1023,
-1023,1023,1023,1023,1022,1022,1022,1021,1021,1020,1019,1018,1017,1017,1015,1014,
-1013,1012,1011,1009,1008,1006,1004,1003,1001,999,997,995,993,991,989,986,
-984,982,979,977,974,971,968,966,963,960,957,953,950,947,944,940,
-937,933,930,926,922,919,915,911,907,903,899,895,890,886,882,878,
-873,869,864,860,855,850,846,841,836,831,826,821,816,811,806,801,
-796,790,785,780,774,769,764,758,753,747,741,736,730,725,719,713,
-707,701,696,690,684,678,672,666,660,654,648,642,636,630,624,617,
-611,605,599,593,587,580,574,568,562,555,549,543,537,530,524,518,
-512,505,499,493,486,480,474,468,461,455,449,443,436,430,424,418,
-412,406,399,393,387,381,375,369,363,357,351,345,339,333,327,322,
-316,310,304,298,293,287,282,276,270,265,259,254,249,243,238,233,
-227,222,217,212,207,202,197,192,187,182,177,173,168,163,159,154,
-150,145,141,137,133,128,124,120,116,112,108,104,101,97,93,90,
-86,83,79,76,73,70,66,63,60,57,55,52,49,46,44,41,
-39,37,34,32,30,28,26,24,22,20,19,17,15,14,12,11,
-10,9,8,6,6,5,4,3,2,2,1,1,1,0,0,0,
-0,0,0,0,1,1,1,2,2,3,4,5,6,6,8,9,
-10,11,12,14,15,17,19,20,22,24,26,28,30,32,34,37,
-39,41,44,46,49,52,55,57,60,63,66,70,73,76,79,83,
-86,90,93,97,101,104,108,112,116,120,124,128,133,137,141,145,
-150,154,159,163,168,173,177,182,187,192,197,202,207,212,217,222,
-227,233,238,243,249,254,259,265,270,276,282,287,293,298,304,310,
-316,322,327,333,339,345,351,357,363,369,375,381,387,393,399,406,
-412,418,424,430,436,443,449,455,461,468,474,480,486,493,499,505
-};
+//#define LEDPASSTHROUGH
+
+uint16_t f_updateLed = 0;
+uint8_t read_r = 0;
+uint8_t read_g = 0;
+uint8_t read_b = 0;
+static uint8_t g_trackerState = TRACKER_OFF;
+uint8_t g_oldTrackerState = TRACKER_OFF;
+uint32_t lastIT = 0;
+uint32_t currentIT = 0;
+uint16_t timeThreshold = 500;
 
 uint32_t dma_Blue_Destination = (uint32_t) &(TIM1->CCR1);
 uint32_t dma_Green_Destination = (uint32_t) &(TIM1->CCR2);
@@ -138,52 +112,81 @@ int main(void)
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
   HAL_TIM_OC_Start(&htim3,TIM_CHANNEL_1);
-  HAL_DMA_Start_IT(&hdma_tim3_ch1_trig,(uint32_t)SineLookup,dma_Blue_Destination,512);
-  HAL_DMA_Start_IT(&hdma_tim3_ch3,(uint32_t)SineLookup,dma_Green_Destination,512);
-  HAL_DMA_Start_IT(&hdma_tim3_ch4_up,(uint32_t)SineLookup,dma_Red_Destination,512);
-  __HAL_TIM_ENABLE_DMA(&htim3,TIM_DMA_CC1);
-  __HAL_TIM_ENABLE_DMA(&htim3,TIM_DMA_CC3);
-  __HAL_TIM_ENABLE_DMA(&htim3,TIM_DMA_CC4);
+  HAL_DMA_Start_IT(&hdma_tim3_ch1_trig,(uint32_t)BreathingLookup,dma_Blue_Destination,512);
+  HAL_DMA_Start_IT(&hdma_tim3_ch3,(uint32_t)BreathingLookup,dma_Green_Destination,512);
+  HAL_DMA_Start_IT(&hdma_tim3_ch4_up,(uint32_t)BreathingLookup,dma_Red_Destination,512);
+  //__HAL_TIM_ENABLE_DMA(&htim3,TIM_DMA_CC1);
+  //__HAL_TIM_ENABLE_DMA(&htim3,TIM_DMA_CC3);
+  //__HAL_TIM_ENABLE_DMA(&htim3,TIM_DMA_CC4);
   
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  setRGB(1023,1023,1023);
+  setRGB(1024,1024,1024);
+  __HAL_TIM_ENABLE_DMA(&htim3,TIM_DMA_CC1);
+  __HAL_TIM_ENABLE_DMA(&htim3,TIM_DMA_CC3);
+  HAL_Delay(500);
+
+  timeThreshold = 0;
+  HAL_GPIO_EXTI_Callback(GPIO_PIN_1);
+  timeThreshold = 250;
   while (1)
   {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    //   if (f_updateLed){
-    //   setRGB(1023,1023,1023);
-    //   if(!read_b){
-    //     setBlue(255);
-    //   }
-    //   if(!read_r){
-    //     setRed(255);
-    //   }
-    //   if(!read_g){
-    //     setGreen(255);
-    //   }
-    //   f_updateLed = 0;
-    // }
+    #ifdef LEDPASSTHROUGH
+      if (f_updateLed){
+      setRGB(1023,1023,1023);
+      if(!read_b){
+        setBlue(255);
+      }
+      if(!read_r){
+        setRed(255);
+      }
+      if(!read_g){
+        setGreen(255);
+      }
+      f_updateLed = 0;
+    }
+    #else
+      if(f_updateLed){
+        //setRGB(1023,1023,1023);
+        switch(g_trackerState){
+          case TRACKER_PAIRING:
+            setRGB(1024,1024,1024);
+            resetDMAIT((uint32_t)BlinkLookup);
+            __HAL_TIM_ENABLE_DMA(&htim3,TIM_DMA_CC1);
+            __HAL_TIM_ENABLE_DMA(&htim3,TIM_DMA_CC3);
+            __HAL_TIM_ENABLE_DMA(&htim3,TIM_DMA_CC4);
+          break;
+          case TRACKER_CONNECTED:
+            __HAL_TIM_DISABLE_DMA(&htim3,TIM_DMA_CC1);
+            __HAL_TIM_DISABLE_DMA(&htim3,TIM_DMA_CC3);
+            __HAL_TIM_DISABLE_DMA(&htim3,TIM_DMA_CC4);
+            setRGB(1024,100,100);
+          break;
+          case TRACKER_ON:
+            setRGB(1024,1024,1024);
+            resetDMAIT((uint32_t)BreathingLookup);
+            __HAL_TIM_DISABLE_DMA(&htim3,TIM_DMA_CC1);
+            __HAL_TIM_DISABLE_DMA(&htim3,TIM_DMA_CC3);
+            __HAL_TIM_DISABLE_DMA(&htim3,TIM_DMA_CC4);
+            __HAL_TIM_ENABLE_DMA(&htim3,TIM_DMA_CC1);
+            __HAL_TIM_ENABLE_DMA(&htim3,TIM_DMA_CC3);
+          break;
+          case TRACKER_NOT_CONNECTED:
+            __HAL_TIM_DISABLE_DMA(&htim3,TIM_DMA_CC1);
+            __HAL_TIM_DISABLE_DMA(&htim3,TIM_DMA_CC3);
+            __HAL_TIM_DISABLE_DMA(&htim3,TIM_DMA_CC4);
+            setRGB(100,100,100);
+          break;
+        }
+      f_updateLed = 0;
+      }
+    #endif
     __WFI();
-    // for(int i = 0; i < 1023; i ++){
-    //   //HAL_Delay(10);
-    //   setBlue(i);
-    // }
-    // setRGB(1023,1023,1023);
-    // for(int i = 0; i < 1023; i ++){
-    //   //HAL_Delay(10);
-    //   setRed(i);
-    // }
-    // setRGB(1023,1023,1023);
-    // for(int i = 0; i < 1023; i ++){
-    //   //HAL_Delay(10);
-    //   setGreen(i);
-    // }
-    // setRGB(1023,1023,1023);
   }
   /* USER CODE END 3 */
 }
@@ -227,10 +230,51 @@ void SystemClock_Config(void)
 /* USER CODE BEGIN 4 */
 void HAL_GPIO_EXTI_Callback(uint16_t Pin)
 {
-  f_updateLed = 1;
+  g_oldTrackerState = g_trackerState;
+  currentIT = HAL_GetTick();
+  uint32_t deltaTimeIT = currentIT - lastIT;
   read_r = HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_7);
   read_g = HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_6);
   read_b = HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_5);
+
+  if(deltaTimeIT <= timeThreshold)
+  {
+    if (read_b == 0 && read_r && read_g){
+    g_trackerState = TRACKER_PAIRING;
+    }
+    else if (read_b && read_g && read_r){
+      g_trackerState = TRACKER_PAIRING;
+    }
+  }
+  else
+  {
+    if(read_g == 0 && read_r && read_b)
+    {
+      g_trackerState = TRACKER_CONNECTED;
+    }
+    else if(read_b == 0 && read_r && read_g )
+    {
+      g_trackerState = TRACKER_NOT_CONNECTED;
+    }
+    else if (read_b && read_g && read_r ){
+      g_trackerState = TRACKER_ON;
+    }
+  }
+  
+  lastIT = currentIT;
+  if(g_oldTrackerState != g_trackerState)
+  {
+      f_updateLed = 1;
+  }
+}
+
+void resetDMAIT(uint32_t lookupTable){
+  HAL_DMA_Abort_IT(&hdma_tim3_ch1_trig);
+  HAL_DMA_Abort_IT(&hdma_tim3_ch3);
+  HAL_DMA_Abort_IT(&hdma_tim3_ch4_up);
+  HAL_DMA_Start_IT(&hdma_tim3_ch1_trig,(uint32_t)lookupTable,dma_Blue_Destination,512);
+  HAL_DMA_Start_IT(&hdma_tim3_ch3,(uint32_t)lookupTable,dma_Green_Destination,512);
+  HAL_DMA_Start_IT(&hdma_tim3_ch4_up,(uint32_t)lookupTable,dma_Red_Destination,512);
 }
 /* USER CODE END 4 */
 
